75516b126524dedac43fd54471903b43c78e0376
Scott <scott.breen221@gmail.com>
2013-03-14 02:14:00 -0400

    Clock Edits ATT:1

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -353,7 +353,8 @@ static inline unsigned int cpufreq_quick_get(unsigned int cpu)
 #define LOW_MAX_FREQ_LIMIT 1188000
 
 #define MIN_FREQ_LIMIT 192000
-#define MAX_FREQ_LIMIT 1512000
+//#define MAX_FREQ_LIMIT 1512000
+#define MAX_FREQ_LIMIT 1969000
 
 enum {
 	SET_MIN = 0,
