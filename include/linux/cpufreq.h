4029f1a0c274f71837606fc8000d9b51599bbf9c
Scott <scott.breen221@gmail.com>
2013-03-30 03:37:30 -0400

    max clock change

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -354,7 +354,7 @@ static inline unsigned int cpufreq_quick_get(unsigned int cpu)
 
 #define MIN_FREQ_LIMIT 192000
 //#define MAX_FREQ_LIMIT 1512000
-#define MAX_FREQ_LIMIT 1512000
+#define MAX_FREQ_LIMIT 1620000
 
 enum {
 	SET_MIN = 0,
